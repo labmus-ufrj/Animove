508e6dc2669d2c6e46e4635f42ef306005c75c9b
Murilo Spineli <96277238+mainSpi@users.noreply.github.com>
2025-10-27 13:18:59 -0300

    new hierarchy

--- src/main/java/labmus/zebrafish_utils/ZFConfigs.java
+++ src/main/java/labmus/zebrafish_utils/ZFConfigs.java
@@ -17,19 +17,27 @@ public abstract class ZFConfigs {
     public static final String ffmpeg = Loader.load(org.bytedeco.ffmpeg.ffmpeg.class);
 
     private static final String prePath = "ZF Utils>";
-    public static final String ffmpegPath = prePath + "FFmpeg";
-    public static final String avgPath = prePath + "Z project";
-    public static final String imgCalcPath = prePath + "Image Calculator";
-    public static final String helperPath = prePath + "Helper";
+    private static final String toolsPath = prePath + "Tools>";
+    private static final String processingPath = prePath + "Processing>"; // processing / generation / some other word
+    private static final String analysisPath = prePath + "Analysis>";
 
-    public static final String heatmapImage = prePath + "Heatmap Analysis>Generate Heatmap Images";
-    public static final String heatmapVideo = prePath + "Heatmap Analysis>Generate Heatmap Videos";
+    public static final String ffmpegPath = toolsPath + "FFmpeg";
+    public static final String avgPath = toolsPath + "Z project";
+    public static final String imgCalcPath = toolsPath + "Image Calculator";
+    public static final String helperPath = toolsPath + "Helper";
 
-    private static final String embryosPath = prePath + "Embryos Analysis>";
+    public static final String heatmapImages = processingPath + "Generate Heatmap Images";
+    public static final String heatmapVideo = processingPath + "Generate Heatmap Video";
+
+    private static final String embryosPath = analysisPath + "Embryos Analysis>";
     public static final String roisPath = embryosPath + "Create ROIs";
-    public static final String analyzeEmbryos = embryosPath + "Analyze";
+    public static final String analyzeEmbryos = embryosPath + "Quantify Heatmap";
+    public static final String analyzeFrequencyEmbryos = embryosPath + "Extract Frequency Data";
 
-    public static final String scorePath = prePath + "Score Analysis";
+    public static final String scorePath = analysisPath + "Calculate Score Gradient";
+    public static final String scoreSectorPath = analysisPath + "Calculate Score Sectors";
+
+    public static final String trackingDataPath = analysisPath + "Extract Tracking Data";
 
     private static final String minimalRequiredVersion = "1.5.10";
     private static final String components = "ffmpeg, opencv";
