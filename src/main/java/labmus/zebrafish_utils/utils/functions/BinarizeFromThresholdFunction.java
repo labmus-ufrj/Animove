baa3cceebab91a73bc47ab4a977174a0dbb031d2
Murilo Spineli <96277238+mainSpi@users.noreply.github.com>
2025-11-18 17:10:19 -0300

    Update BinarizeFromThresholdFunction.java

--- src/main/java/labmus/zebrafish_utils/utils/functions/BinarizeFromThresholdFunction.java
+++ src/main/java/labmus/zebrafish_utils/utils/functions/BinarizeFromThresholdFunction.java
@@ -7,6 +7,9 @@ import java.util.function.Function;
 public class BinarizeFromThresholdFunction implements Function<Mat, Mat> {
     @Override
     public Mat apply(Mat mat) {
+//        -> iterar sobre frames
+//                - binarizar o frame (normalizar entre 0 e 1 ou fazer um threshold)
+//        - acumular o darkest
         return null;
     }
 }
