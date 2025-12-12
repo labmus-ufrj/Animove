fb92671e785be70472b4b1351de89ad2acc401a4
Murilo Spineli <96277238+mainSpi@users.noreply.github.com>
2025-12-12 15:17:22 -0300

    locale missing

--- src/main/java/labmus/animove/analysis/QuantifyHeatmap.java
+++ src/main/java/labmus/animove/analysis/QuantifyHeatmap.java
@@ -117,7 +117,7 @@ public class QuantifyHeatmap implements Command, Interactive {
                     double index = (double) blackPixelCount / roiPixelCount;
 
                     rt.setValue("ROI Name", i, roi.getName());
-                    rt.setValue("Area %", i, String.format("%.2f", index * 100));
+                    rt.setValue("Area %", i, String.format(Locale.US, "%.2f", index * 100));
                     dataset.addValue(index * 100, "", roi.getName());
                 }
             }
